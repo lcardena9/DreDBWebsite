0adff7f896777a5755b07cf0eb1b64fbec626109
lcardena9 <42814907+lcardena9@users.noreply.github.com>
2018-11-28 21:27:52 -0800

    fixed email error

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6251c27660377740380436a286960497.js"
+  "/precache-manifest.67f51a5b13426f0e3b216285ec8b86fe.js"
 );
 
 workbox.clientsClaim();
